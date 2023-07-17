5ef3985b0e3d6f7595df8ec980d80c2d86416363
Christopher Moore <christopher.e.moore@gmail.com>
2023-07-17 21:05:23 +0700

    ext.file -> ext.path

--- allspacing.lua
+++ allspacing.lua
@@ -4,8 +4,8 @@ for p=2,15 do
 	for n=0,p do
 		for m=0,n do
 			if os.execute('./spacing.lua '..m..' '..n..' '..p..' > tmp') then
-				local tmp = file'tmp':read()
-				file'tmp':remove()
+				local tmp = path'tmp':read()
+				path'tmp':remove()
 				local lines = tmp:trim():split'\n'
 				assert(lines[#lines] == 'found')
 				local result = lines[#lines-1]
