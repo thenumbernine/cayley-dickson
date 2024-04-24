7253dc7ffc3ae4a04fa153e8e4d90a2ab925dcc3
Christopher Moore <christopher.e.moore@gmail.com>
2024-04-24 23:37:07 +0800

    more

--- viz-manual.lua
+++ viz-manual.lua
@@ -2,6 +2,7 @@
 local ffi = require 'ffi'
 local bit = require 'bit'
 local gl = require 'gl'
+local template = require 'template'
 local vec3f = require 'vec-ffi.vec3f'
 local vec3d = require 'vec-ffi.vec3d'
 local quatd = require 'vec-ffi.quatd'
@@ -28,50 +29,60 @@ function App:initGL()
 ..'precision highp float;\n'
 
 	self.program = GLProgram{
-		vertexCode = self.shaderHeader..[[
+		vertexCode = self.shaderHeader..template[[
 in vec3 vertex;
-out vec3 eyeposv;
-out vec3 normalv;
+out vec3 mvposv;
 uniform mat4 mvMat;
 uniform mat4 projMat;
+
+const float M_PI = <?=('%.49f'):format(math.pi)?>;
+
+vec4 quatFromAngleAxis(vec4 aa) {
+	return vec4(sin(.5 * aa.w) * normalize(aa.xyz), cos(.5 * aa.w));
+}
+
+vec3 rotate(vec4 q, vec3 v) {
+	return v + 2. * cross(q.xyz, cross(q.xyz, v) + q.w * v); 
+}
+
 void main() {
 	// TODO inv mat to get eyepos?
-	vec4 mvpos = mvMat * vec4(vertex, 1.);
-	normalv = cross(dFdx(mvpos), dFdy(mvpos));
-	eyeposv = normalize((mvMat * vec4(0., 0., 1., 1.)).xyz);
+	
+	vec4 qy = quatFromAngleAxis(vec4(0., 1., 0., M_PI * vertex.x * 2.));
+	vec4 qz = quatFromAngleAxis(vec4(0., 0., 1., M_PI * vertex.x));
+	
+	const float radius = 1.;
+	const float width = .5;
+	vec3 worldpos = rotate(qy,
+		rotate(qz, vec3(0., .5 * width * (2. * vertex.y - 1.), 0.))
+		+ vec3(radius, 0., 0.)
+	);
+
+	vec4 mvpos = mvMat * vec4(worldpos, 1.);
+	mvposv = mvpos.xyz;
 	gl_Position = projMat * mvpos;
 }
 ]],
 		fragmentCode = self.shaderHeader..[[
 const vec4 color = vec4(1., 0., 0., 1.);
 in vec3 eyeposv;
-in vec3 normalv;
+in vec3 mvposv;
 out vec4 fragColor;
 void main() {
-	fragColor = abs(normalv.z) * color;
+	vec3 n = normalize(cross(dFdx(mvposv), dFdy(mvposv)));
+	fragColor = abs(n.z) * color;
 }
 ]],
 	}:useNone()
 
 	self.vertexCPU = vector_vec3f()
 
-	local radius = 1
-	local width = .5
 	local n = 360
 	-- TODO why does emplace_back()[0] crash?
 	self.vertexCPU:resize(2 * (n+1))
 	for i=0,n do
 		for z=0,1 do
-			local s = i/n
-			local qy = quatd():fromAngleAxis(0,1,0, s*360)
-			local qz = quatd():fromAngleAxis(0,0,1, s*180)
---print('size', self.vertexCPU:size())
-			self.vertexCPU.v[z + 2 * i]:set(
-				qy:rotate(
-					qz:rotate(vec3d(0, .5*width*(2*z-1), 0))
-					+ vec3d(radius, 0, 0)
-				):unpack()
-			)
+			self.vertexCPU.v[z + 2 * i]:set(i/n, z, 0)
 		end
 	end
 
@@ -94,6 +105,8 @@ void main() {
 			vertex = self.vertexGPU,
 		},
 	}
+
+	gl.glEnable(gl.GL_DEPTH_TEST)
 end
 
 function App:update()
