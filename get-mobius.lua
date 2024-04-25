be471e4bcdfcc947f31cd92a981b39f26151d185
Christopher Moore <christopher.e.moore@gmail.com>
2024-04-25 15:29:05 +0800

    more

--- get-mobius.lua
+++ get-mobius.lua
@@ -23,8 +23,7 @@ for i,t in ipairs(allTriplets) do
 	print('t'..i, table.unpack(t))
 end
 
-
-local function tripletsOf(e)
+local function getQuaternionTriplets(e)
 	local ts = table()
 	for _,t in ipairs(allTriplets) do
 		for j=1,3 do
@@ -54,7 +53,7 @@ es:remove(1)
 
 for i,e in ipairs(es) do
 	io.write('triplets of e'..shortname(e)..':')
-	for _,t in ipairs(tripletsOf(e)) do
+	for _,t in ipairs(getQuaternionTriplets(e)) do
 		io.write(' '..t:mapi(shortname):concat'')
 	end
 	print()
@@ -88,8 +87,8 @@ local function tablesAreEqual(a,b)
 end
 
 -- offsets of indexes to form triplets within the mobius strip
-local offsets = table{0,1,3}
-local function fillMobius(...)
+local quaternionInOctonionOffsets = table{0,1,3}
+local function fillMobius(offsets, getSub, ...)
 	local initM = table{...}
 	return coroutine.wrap(function()
 		local function recurse(m)
@@ -101,24 +100,29 @@ local function fillMobius(...)
 					end
 					return
 				elseif not m[i+offsets[2]] then
-					for _,t in ipairs(tripletsOf(m[i+offsets[1]])) do
+					for _,t in ipairs(getSub(m[i+offsets[1]])) do
 						if not m:find(nil, function(t2) return t2.index == t[2].index end) then
 							m[i+offsets[2]] = t[2]
 							recurse(table(m))
 						end
 					end
 					return
-				elseif not m[i+offsets[3]] then
-					local t3 = m[i+offsets[1]] * m[i+offsets[2]]
-					for j=1,#m-offsets[3]-1 do
-						if tablesAreEqual(m:sub(j,j+offsets:last()), m:sub(#m-offsets:last(),#m)) then
-							coroutine.yield(m:sub(1,#m-offsets[3]-1))
-							return 
+				else
+					for k=3,#offsets do
+						if not m[i+offsets[k]] then
+							-- TODO don't use mul ... use pre-stored sets ...
+							local t3 = m[i+offsets[k-2]] * m[i+offsets[k-1]]
+							for j=1,#m-offsets[k]-1 do
+								if tablesAreEqual(m:sub(j,j+offsets[k]), m:sub(#m-offsets[k],#m)) then
+									coroutine.yield(m:sub(1,#m-offsets[k]-1))
+									return 
+								end
+							end
+							m[i+offsets[k]] = t3
+							recurse(m)
+							return
 						end
 					end
-					m[i+offsets[3]] = t3
-					recurse(m)
-					return
 				end
 			end
 		end
@@ -138,21 +142,67 @@ end)
 local dontOmitIsomorphisms = false
 
 local function mobiusStrips()
-	local all = table()
+	local allOctonionsInSedenion = table()
 	local uniqueset = table()
-	for m in fillMobius(initM:unpack()) do
+	for m in fillMobius(quaternionInOctonionOffsets, getQuaternionTriplets, initM:unpack()) do
 		local key = m:mapi(function(e) return e.index end):sort():concat'_'
 		if dontOmitIsomorphisms or not uniqueset[key] then
-			all:insert(m)
+			allOctonionsInSedenion:insert(m)
 			uniqueset[key] = true
 		end
 	end
-	return all
+	return allOctonionsInSedenion
 end
 
-local all = mobiusStrips()
-for i,m in ipairs(all) do
+local allOctonionsInSedenion = mobiusStrips()
+for i,m in ipairs(allOctonionsInSedenion) do
 	print('ring #'..hex(i)..': '..m:mapi(shortname):concat' ')
 end
 
-print('number of unique mobius rings:',#all)
+print('number of unique mobius rings:',#allOctonionsInSedenion)
+
+-- now remove the trailing minus-parity rings
+print('sign-agnostic:')
+for i=1,#allOctonionsInSedenion do
+	local m = allOctonionsInSedenion[i]
+	if #m == 14 then
+		for j=1,7 do
+			assert(m[j+7].index == m[j].index)
+		end
+		m = table.sub(m, 1, 7)
+		allOctonionsInSedenion[i] = m
+	end
+	-- TODO make abs here
+	for j=1,#m do
+		m[j].negative = false
+	end
+end
+for i,m in ipairs(allOctonionsInSedenion) do
+	print('ring #'..hex(i)..': '..m:mapi(shortname):concat' ')
+end
+-- TODO fill sedenion mobius ...
+local octonionInSedenionOffsets = {0, 1, 2, 4, 5, 8, 10} 
+local function getSubOctonions(e)
+	local ts = table()
+	for _,t in ipairs(allOctonionsInSedenion) do
+		for j=1,7 do
+			if t[j].index == e.index then
+				ts:insert(table{
+					t[j],
+					t[j%7+1],
+					t[(j+1)%7+1],
+					t[(j+2)%7+1],
+					t[(j+3)%7+1],
+					t[(j+4)%7+1],
+					t[(j+5)%7+1],
+				})
+				break
+			end
+		end
+	end
+	return ts
+
+end
+for m in fillMobius(octonionInSedenionOffsets, getSubOctonions, 1, 2, 4, 3) do
+	print(m:concat' ')
+end
